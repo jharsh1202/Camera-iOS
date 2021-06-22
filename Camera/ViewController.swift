0f0caeabe6b308f68bd86bad5c176ce72c364e09
Harshit Jain <harshit120299@gmail.com>
2021-06-22 16:50:42 +0530

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  PhotoLibrary
+//
+//  Created by Harshit Jain on 22/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagePicker = UIImagePickerController()
+    
+    @IBOutlet weak var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+}
+
